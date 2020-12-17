3c03897f0dc673a95ff93861f3e4dc4f217e969f
Ricol Wang <ricol@opensimsim.com>
2020-12-17 21:35:40 +0800

    update

---
+++ math/show_primes.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/env python
+
+def is_prime(num):
+	if num <= 1: return False
+	if num != 2 and num % 2 == 0: return False
+
+	to = num / 2 + 1
+	for x in range(3, to, 2):
+		if num % x == 0: return False
+	return True
+
+if __name__ == "__main__":
+	primes = []
+	for x in range(2, 100):
+		if is_prime(x): primes.append(x)
+	print(primes)
+	
+	product = primes[0]
+	for i in range(1, len(primes)):
+		print("prime: " + str(primes[i]))
+		new_prime = product * primes[i] + 1
+		if is_prime(new_prime):
+			print("new prime: " + str(new_prime))
+		else:
+			print(str(new_prime) + " is not prime! [error]")
+		product *= primes[i]
