0cc7344cd851422b25fecda7fcb8fe2eaf2a4dd9
Ricol Wang <ricol@opensimsim.com>
2023-09-26 17:43:56 +0800

    update

---
+++ generator/producer_consumer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import random
+
+def producer():
+  while True:
+    data = random.randint(0, 9)
+    print("producing...", data)
+    yield data
+
+def consumer():
+  count = 0
+  while True:
+    count += 1
+    print("consumer...%d..." % count)
+    data = yield
+    print("consumed...", data)
+
+def clerk(jobs, producer, consumer):
+  print('execute () times of production and consumption'.format(jobs))
+  p = producer()
+  c = consumer()
+  next(c)
+  for i in range(jobs):
+    data = next(p)
+    c.send(data)
+
+if __name__ == "__main__":
+  clerk(10, producer, consumer)
+  print("end main.")
